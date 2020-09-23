146fa180c50d2634fc5401736804dd39d284865f
Wanderson César <contato@wandersoncesar.com.br>
2020-09-23 09:15:55 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# simple-credit-card-validator
+A simple credit card validator
+## How to install
+`npm install --save simple-credit-card-validator`
+## How to use
+``var validator = require("simple-credit-card-validator")``
+``var isValidEmail = validator("5496524458927653") // true``
+``var isValidEmail = validator("1234567891234567") // false``
---
+++ app.js
@@ -0,0 +1,16 @@
+function isValidEmail(number) {
+    var val;
+    var bit = 1;
+    var sum = 0;
+    var len = numeroCartao.length;
+    var arr = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+
+    while (len) {
+        val = parseInt(number.charAt(--len), 10);
+        sum += (bit ^= 1) ? arr[val] : val;
+    }
+
+    return sum && sum % 10 === 0;
+}
+
+module.exports = isValidEmail;
